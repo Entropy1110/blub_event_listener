56dbaac7f50a3d1254c488d9465c0b22185249b5
Entropy1110 <entropy1110@gmail.com>
2025-05-30 17:54:43 +0900

    Revert "Event Polling Issue"
    This reverts commit d8be9f2ca13586c7658fa043b606979bc2a8c145.

--- template.config.py
+++ template.config.py
@@ -1,3 +1,3 @@
 HTTP_URL = "https://base-sepolia.g.alchemy.com/v2/<api_key>"
 CONTRACT_ADDRESS = "0xb7Da4867E20dc2205c95F851C87eaf5f8e771a20"
-SK_USER_SECRET = "12345678"
+SK_USER_SECRET = "<your_secret_key>"
